16ab69fa63886286c0b3ed00a422e7e04dab5f29
prashanthvarmahammoq <77702446+prashanthvarmahammoq@users.noreply.github.com>
2022-02-03 23:53:59 +0100

    initial commit

--- vector-add/vector-add.cu
+++ vector-add/vector-add.cu
@@ -24,15 +24,36 @@ static void checkCudaCall(cudaError_t result) {
     }
 }
 
-__global__ void vectorAddKernel(float* A, float* B, float* Result) {
+__global__ void vectorAddKernel(int n,float* A, float* B, float* Result) {
     // insert operation here
-    int i = threadIdx.x + blockDim.x * blockIdx.x;
-    C[i] = A[i] + B[i];
-    if (i<N) C[i] = A[i] + B[i];
+        int i = threadIdx.x + blockDim.x * blockIdx.x;
+        if (i<n)  Result[i]= A[i] + B[i];
 }
 
-void vectorAddCuda(int n, float* a, float* b, float* result) {
+__global__ void vectorSubKernel(int n,float* A, float* B, float* Result) {
+    // insert operation here
+        int i = threadIdx.x + blockDim.x * blockIdx.x;
+        if (i<n)  Result[i]= A[i] - B[i];
+}
+
+__global__ void vectorMulKernel(int n,float* A, float* B, float* Result) {
+    // insert operation here
+        int i = threadIdx.x + blockDim.x * blockIdx.x;
+        if (i<n)  Result[i]= A[i] * B[i];
+}
+
+__global__ void vectorDivKernel(int n,float* A, float* B, float* Result) {
+    // insert operation here
+        int i = threadIdx.x + blockDim.x * blockIdx.x;
+        if (i<n)  Result[i]= A[i] / B[i];
+}
+
+
+
+void vectorOpsCuda(int n,int operation,int gridGeoThreadBlock, float* a, float* b, float* result) {
     int threadBlockSize = 512;
+
+    if (gridGeoThreadBlock!=0) threadBlockSize = gridGeoThreadBlock;
 
     // allocate the vectors on the GPU
     float* deviceA = NULL;
@@ -66,7 +87,10 @@ void vectorAddCuda(int n, float* a, float* b, float* result) {
     high_resolution_clock::time_point t2 = high_resolution_clock::now();
 
     // execute kernel
-    vectorAddKernel<<<n/threadBlockSize, threadBlockSize>>>(deviceA, deviceB, deviceResult);
+    if(operation == 0) vectorAddKernel<<<n/threadBlockSize, threadBlockSize>>>(n, deviceA, deviceB, deviceResult);
+    if(operation == 1) vectorSubKernel<<<n/threadBlockSize, threadBlockSize>>>(n, deviceA, deviceB, deviceResult);
+    if(operation == 2) vectorMulKernel<<<n/threadBlockSize, threadBlockSize>>>(n, deviceA, deviceB, deviceResult);
+    if(operation == 3) vectorDivKernel<<<n/threadBlockSize, threadBlockSize>>>(n, deviceA, deviceB, deviceResult);
     cudaDeviceSynchronize();
 
     // check whether the kernel invocation was successful
@@ -83,9 +107,21 @@ void vectorAddCuda(int n, float* a, float* b, float* result) {
     checkCudaCall(cudaFree(deviceB));
     checkCudaCall(cudaFree(deviceResult));
 
-    std::cout << "vector-add (H2D):    \t\t" << duration_cast<microseconds>(t2 - t1).count() << "us" << std::endl;
-    std::cout << "vector-add (kernel): \t\t" << duration_cast<microseconds>(t3 - t2).count() << "us" << std::endl;
-    std::cout << "vector-add (D2H):    \t\t" << duration_cast<microseconds>(t4 - t3).count() << "us" << std::endl;
+    if(operation == 0) std::cout << "vector-add (H2D):    \t\t" << duration_cast<microseconds>(t2 - t1).count() << "us" << std::endl;
+    if(operation == 0) std::cout << "vector-add (kernel): \t\t" << duration_cast<microseconds>(t3 - t2).count() << "us" << std::endl;
+    if(operation == 0) std::cout << "vector-add (D2H):    \t\t" << duration_cast<microseconds>(t4 - t3).count() << "us" << std::endl;
+
+    if(operation == 1) std::cout << "vector-sub (H2D):    \t\t" << duration_cast<microseconds>(t2 - t1).count() << "us" << std::endl;
+    if(operation == 1) std::cout << "vector-sub (kernel): \t\t" << duration_cast<microseconds>(t3 - t2).count() << "us" << std::endl;
+    if(operation == 1) std::cout << "vector-sub (D2H):    \t\t" << duration_cast<microseconds>(t4 - t3).count() << "us" << std::endl;
+
+    if(operation == 2) std::cout << "vector-mul (H2D):    \t\t" << duration_cast<microseconds>(t2 - t1).count() << "us" << std::endl;
+    if(operation == 2) std::cout << "vector-mul (kernel): \t\t" << duration_cast<microseconds>(t3 - t2).count() << "us" << std::endl;
+    if(operation == 2) std::cout << "vector-mul (D2H):    \t\t" << duration_cast<microseconds>(t4 - t3).count() << "us" << std::endl;
+
+    if(operation == 3) std::cout << "vector-div (H2D):    \t\t" << duration_cast<microseconds>(t2 - t1).count() << "us" << std::endl;
+    if(operation == 3) std::cout << "vector-div (kernel): \t\t" << duration_cast<microseconds>(t3 - t2).count() << "us" << std::endl;
+    if(operation == 3) std::cout << "vector-div (D2H):    \t\t" << duration_cast<microseconds>(t4 - t3).count() << "us" << std::endl;
 }
 
 void vectorAddSeq(int n, float* a, float* b, float* result) {
@@ -108,8 +144,11 @@ int main(int argc, char* argv[]) {
     float* b = new float[n];
     float* result = new float[n];
     float* result_s = new float[n];
-
+    int gridGeoThreadBlock = 0;
+    int operation = 0;
     if (argc > 1) n = atoi(argv[1]);
+    if (argc > 2) gridGeoThreadBlock = atoi(argv[2])
+    if (argc > 3) operation = atoi(argv[3])
 
     std::cout << "Adding two vectors of " << n << " integer elements." << std::endl;
 
@@ -119,8 +158,9 @@ int main(int argc, char* argv[]) {
         b[i] = i;
     }
 
-    vectorAddSeq(n, a, b, result_s);
-    vectorAddCuda(n, a, b, result);
+    vectorAddSeq(n,a, b, result_s);
+
+    vectorOpsCuda(n, operation, gridGeoThreadBlock, a, b, result);
 
     // verify the resuls
     for(int i=0; i<n; i++) {
